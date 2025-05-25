99c9cae5831ec59249bbe74834b630ff52bc951b
Beatrice <bea08.baumgartner@gmail.com>
2025-05-25 17:06:08 +0200

    created DTOs

---
+++ library-client/src/DTOs/bookDTO.ts
@@ -0,0 +1,11 @@
+export interface BookDTO {
+    id: number
+    libraryId: number
+    author: string
+    title: string
+    isbn: string
+    image: string
+    publisher: string
+    shortDescription: string
+    publishedYear: number
+}
---
+++ library-client/src/DTOs/libraryDTO.ts
@@ -0,0 +1,5 @@
+export interface LibraryDTO {
+    id: number
+    name: string
+    address: string
+}
