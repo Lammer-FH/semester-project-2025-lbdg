f9629b940d035936b17f3a5b241119ede69b6e5e
Lukasl124 <l.ledermueller@gmx.at>
2025-05-28 19:44:41 +0200

    added userService to get users

---
+++ library-client/src/services/userService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import {User} from "@/models/user";
+
+const API_URL = 'http://localhost:8080/library-system/v1/users';
+
+export const userService = {
+
+    async getUsers(): Promise<User[]> {
+        const response = await axios.get<User[]>(API_URL);
+        return response.data;
+    },
+
+};
