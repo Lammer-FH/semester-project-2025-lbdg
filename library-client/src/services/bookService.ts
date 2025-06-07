3fe48f919d12c6cdcb8b95e4be729ae75da2baf3
Lukasl124 <l.ledermueller@gmx.at>
2025-06-07 14:10:06 +0200

    added service functions needed for create,update,delete book

--- library-client/src/services/bookService.ts
+++ library-client/src/services/bookService.ts
@@ -15,4 +15,37 @@ export const bookService = {
         const response = await axios.get<Rating[]>(`${API_URL}/${bookId}/ratings`);
         return response.data;
     },
+
+    async getBookEditDetails(id: number): Promise<Book> {
+        const response = await axios.get<Book>(`${API_URL}/${id}/edit`);
+        return response.data;
+    },
+
+    async createBook(book: Pick<Book, any>): Promise<number> {
+        try {
+            const response = await axios.post<number>(`${API_URL}`, book);
+            return response.data;
+        } catch (error) {
+            return 0;
+        }
+
+    },
+
+    async editBook(bookId: number, book: Pick<Book, any>): Promise<boolean> {
+        try {
+            await axios.put(`${API_URL}/${bookId}`, book);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    },
+
+    async deleteBook(bookId: number): Promise<boolean> {
+        try {
+            await axios.delete(`${API_URL}/${bookId}`);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    },
 };
