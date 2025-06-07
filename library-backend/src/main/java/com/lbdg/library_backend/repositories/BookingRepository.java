18fe5a64a1d088ea1ab816f259d5ce45a2a9a092
Gabriel <wiedermann.gabriel@gmail.com>
2025-06-07 08:46:12 +0200

    added the method findByBookEntity which returns all the bookings to a book

--- library-backend/src/main/java/com/lbdg/library_backend/repositories/BookingRepository.java
+++ library-backend/src/main/java/com/lbdg/library_backend/repositories/BookingRepository.java
@@ -1,12 +1,15 @@
 package com.lbdg.library_backend.repositories;
 
 import com.lbdg.library_backend.entities.BookingEntity;
+import com.lbdg.library_backend.entities.RatingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
     @Query("SELECT COUNT(b) = 0 FROM BookingEntity b WHERE :currentDate BETWEEN b.startDate AND b.endDate AND b.bookEntity.id = :bookId")
     boolean isBookCurrentlyAvailable(LocalDate currentDate, Long bookId);
+    List<BookingEntity> findByBookEntityId(Long bookId);
 }
