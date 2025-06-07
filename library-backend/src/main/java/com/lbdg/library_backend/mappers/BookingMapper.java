45b4b5caf766728abce032cef4695d8d24979d3c
Gabriel <wiedermann.gabriel@gmail.com>
2025-06-07 12:08:19 +0200

    created Mapper for Bookings

---
+++ library-backend/src/main/java/com/lbdg/library_backend/mappers/BookingMapper.java
@@ -0,0 +1,33 @@
+package com.lbdg.library_backend.mappers;
+
+import com.lbdg.library_backend.DTOs.requestDTOs.BookingCreateRequestDTO;
+import com.lbdg.library_backend.DTOs.requestDTOs.BookingEditRequestDTO;
+import com.lbdg.library_backend.DTOs.responseDTOs.BookingEditResponseDTO;
+import com.lbdg.library_backend.entities.BookEntity;
+import com.lbdg.library_backend.entities.BookingEntity;
+import com.lbdg.library_backend.entities.UserEntity;
+
+public class BookingMapper {
+    public static BookingEditResponseDTO toBookingEditResponseDTO(BookingEntity bookingEntity) {
+        return BookingEditResponseDTO.builder()
+                .userFullName(bookingEntity.getUserEntity().getFirstName() + " " + bookingEntity.getUserEntity().getLastName())
+                .startDate(bookingEntity.getStartDate())
+                .endDate(bookingEntity.getEndDate())
+                .build();
+    }
+
+    public static void updateBookingEntityFromBookingEditRequestDTO(BookingEntity bookingEntity, BookingEditRequestDTO bookingEditRequestDTO) {
+        bookingEntity.setStartDate(bookingEditRequestDTO.getStartDate());
+        bookingEntity.setEndDate(bookingEditRequestDTO.getEndDate());
+    }
+
+    public static BookingEntity toBookingEntity(BookingCreateRequestDTO bookingCreateRequestDTO, BookEntity bookEntity, UserEntity userEntity) {
+        return BookingEntity.builder()
+                .bookEntity(bookEntity)
+                .userEntity(userEntity)
+                .startDate(bookingCreateRequestDTO.getStartDate())
+                .endDate(bookingCreateRequestDTO.getEndDate())
+                .build();
+
+    }
+}
