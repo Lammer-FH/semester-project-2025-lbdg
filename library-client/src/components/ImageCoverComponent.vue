cc22eee11183c3734f154bfaa6618bcbcb2e3396
Gabriel <wiedermann.gabriel@gmail.com>
2025-06-09 20:19:17 +0200

    created an own Component for the Image Cover

---
+++ library-client/src/components/ImageCoverComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <img
+      :src="defaultCover"
+      alt="Cover"
+      class="detail-image"
+  />
+</template>
+
+<script setup lang='ts'>
+//TODO: add bookCover as Input, use default cover as fallback.
+  import defaultCover from '../../assets/default_book_cover.jpg'
+</script>
+
+<style>
+  .detail-image {
+    width: 100px;
+    height: auto;
+    border-radius: 4px;
+    object-fit: cover;
+  }
+</style>
