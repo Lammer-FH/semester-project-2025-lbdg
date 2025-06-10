e83c97624ff27e8062c124ca971c51577a0d7d92
Lukasl124 <l.ledermueller@gmx.at>
2025-06-10 22:58:58 +0200

    added DTO and Mapper Method for students

--- library-backend/src/main/java/com/lbdg/library_backend/mappers/UserMapper.java
+++ library-backend/src/main/java/com/lbdg/library_backend/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package com.lbdg.library_backend.mappers;
 
 
 import com.lbdg.library_backend.DTOs.responseDTOs.UserResponseDTO;
+import com.lbdg.library_backend.DTOs.responseDTOs.UserStudentResponseDTO;
 import com.lbdg.library_backend.entities.UserEntity;
 
 public class UserMapper {
@@ -12,6 +13,14 @@ public class UserMapper {
                 .id(userEntity.getId())
                 .userFullName(userEntity.getFirstName() + " " + userEntity.getLastName())
                 .role(userEntity.getRole().toString())
+                .build();
+    }
+
+    public static UserStudentResponseDTO toUserStudentResponseDTO(UserEntity userEntity) {
+
+        return UserStudentResponseDTO.builder()
+                .id(userEntity.getId())
+                .userFullName(userEntity.getFirstName() + " " + userEntity.getLastName())
                 .build();
     }
 }
